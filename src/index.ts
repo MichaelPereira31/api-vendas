a9dbbbb35b68679d2766876796a484be59a1d33a
Michael Pereira <michaelpereira408@gmail.com>
2021-10-06 21:00:47 -0300

    Validacao dos dados de requisicao

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import * as express from "express";
+import { NextFunction, Request, Response } from 'express';
+import { celebrate, errors } from "celebrate";
+import 'express-async-errors';
 import * as bodyParser from "body-parser";
 
 import routes from "./routes";
+import AppError from "./errors/AppError";
+import express = require("express");
+
 
 const app = express()
 createConnection()
 
 app.use(bodyParser.json())
-app.use(routes)
 
-app.listen(8000)
+app.use(routes)
+app.use(errors())
+
+app.use(
+    (error: Error, request: Request, response: Response, next: NextFunction) => {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+  
+      console.log(error);
+  
+      return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+      });
+    },
+  );
+  
+app.listen(8000, () => {
+    console.log('Server started on port 8000! 🏆');
+}
+    )
--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,13 +1,53 @@
 import {Router} from 'express'
 import ProductsController from '../controller/product/ProductsController';
-
+import {celebrate,Joi,Segments} from 'celebrate'
+import { join } from 'path';
 const productsRouter = Router();
 const productsControlller = new ProductsController()
 
 productsRouter.get('/',productsControlller.index);
-productsRouter.get('/:id',productsControlller.show);
-productsRouter.post('/',productsControlller.create);
-productsRouter.put('/:id', productsControlller.update);
-productsRouter.delete('/:id',productsControlller.delete)
+
+productsRouter.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]:{
+            id:Joi.string().uuid().required(),
+        },
+    }),
+    productsControlller.show);
+
+productsRouter.post(
+    '/',
+    celebrate({
+        [Segments.BODY]:{
+            name:Joi.string().required(),
+            price:Joi.number().precision(2).required(),
+            quantity:Joi.number().required(),
+        },
+    }),
+    productsControlller.create);
+
+productsRouter.put(
+    '/:id',
+    celebrate({
+        [Segments.BODY]:{
+            name:Joi.string().required(),
+            price:Joi.number().precision(2).required(),
+            quantity:Joi.number().required(),
+        },
+        [Segments.PARAMS]:{
+            id:Joi.string().uuid().required()
+        }
+    }),
+    productsControlller.update);
+
+productsRouter.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]:{
+            id:Joi.string().uuid().required(),
+        }
+    }),
+    productsControlller.delete);
 
 export default productsRouter;
