a2950b6b5286dffd2d6ef3daf6ac1ff190fcd44d
Michael Pereira <michaelpereira408@gmail.com>
2021-10-15 20:43:00 -0300

    Criando sessao de autenticacao

---
+++ src/modules/user/CreateSessionsService.ts
@@ -0,0 +1,30 @@
+import { compare } from "bcryptjs";
+import { getCustomRepository } from "typeorm";
+import User from "../../entity/User";
+import AppError from "../../errors/AppError";
+import UsersRepository from "../../repositories/UsersRepository";
+
+interface IRequest{
+    email: string;
+    password:string;
+}
+/*
+interface IResponse{
+    user: User;
+}*/
+
+class CreateSessionsService{
+    public async execute({email,password}:IRequest):Promise</*IResponse |*/ User>{
+        const usersRepository = getCustomRepository(UsersRepository)
+        const user = await usersRepository.findByEmail(email);
+        if(!user){
+            throw new AppError('Incorrect email/password combination.',401)
+        }
+        const passwordCorfirmed = await compare(password,user.password)
+        if(!passwordCorfirmed){
+            throw new AppError('Incorrect email/password combination.',401)
+        }
+
+        return user
+    }
+}
